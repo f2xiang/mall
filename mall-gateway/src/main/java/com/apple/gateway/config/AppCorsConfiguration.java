15c24d7d6e20a9ab77569464b6e0f97483e08d59
阿祥乖宝宝🤗 <oncwnuMeWm-JuY-f4UnjAYaFOEto@git.weixin.qq.com>
2020-09-05 14:48:16 +0800

    跨域和三级菜单分类

---
+++ mall-gateway/src/main/java/com/apple/gateway/config/AppCorsConfiguration.java
@@ -0,0 +1,36 @@
+package com.apple.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @description: 系统跨域解决
+ * @author: fengx
+ * @create: 2020-09-05 11:11
+ */
+@Configuration
+public class AppCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 跨域配置
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 允许哪些请求头
+        configuration.addAllowedHeader("*");
+        // 允许哪些请求方式
+        configuration.addAllowedMethod("*");
+        // 来源方式
+        configuration.addAllowedOrigin("*");
+        // 是否允许携带cookie
+        configuration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/*", configuration);
+        return new CorsWebFilter(source);
+    }
+}
--- mall-product/src/main/java/com/apple/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/apple/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.apple.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,44 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> getTreeList() {
+        List<CategoryEntity> list = baseMapper.selectList(null);
+        return list.stream()
+                // 过滤父
+                .filter(item -> item.getParentCid() == 0)
+                // 映射子
+                .map(item -> {
+                    item.setChildren(getChildren(item, list));
+                    return item;
+                })
+                // 排序
+                .sorted((item1, item2) ->
+                        (item2.getSort() == null? 0: item2.getSort())  - (item1.getSort() == null? 0: item1.getSort()))
+                // 数据收集
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * 获取某一个菜单的子菜单
+     * @param root 父菜单
+     * @param all  所有菜单
+     * @return 子菜单
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream()
+                .filter(item -> Objects.equals(item.getParentCid(), root.getCatId()))
+                // 递归 获取 这个分类下的子分类
+                .map(item -> {
+                    item.setChildren(getChildren(item, all));
+                    return item;
+                })
+                .sorted((item1, item2) ->
+                        (item2.getSort() == null? 0: item2.getSort())  - (item1.getSort() == null ? 0: item1.getSort()))
+                .collect(Collectors.toList());
     }
 
 }
