1f8ab9ea2c5de7def0b606c9a242c2967eedff13
阿祥乖宝宝🤗 <oncwnuMeWm-JuY-f4UnjAYaFOEto@git.weixin.qq.com>
2021-05-01 17:31:41 +0800

    sentinel配置文件

--- mall-common/src/main/java/com/apple/common/exception/BizCodeEnum.java
+++ mall-common/src/main/java/com/apple/common/exception/BizCodeEnum.java
@@ -9,6 +9,7 @@ package com.apple.common.exception;
 public enum BizCodeEnum {
     UNKNOW_EXCEPTION(10000, "系统未知异常"),
     VALID_EXCEPTION(10001, "参数格式校验异常"),
+    TOO_MANY_REQUEST(10002, "请求过多，挤爆了"),
     ;
 
     private Integer code;
